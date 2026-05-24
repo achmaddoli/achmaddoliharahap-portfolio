9129749010feee2427fe058c0df7d27e7ba0d790
Achmad Doli Harahap <doliachmad406@gmail.com>
2026-05-24 07:09:01 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
           </p>
 
           <div className="flex items-center sm:gap-4 gap-2">
-            <a href="https://drive.google.com/file/d/1z3dbsVM9xiWcqi6wW5mu2-MLl1KdKQ19/view?usp=sharing" className="bg-violet-700 p-4 rounded-2xl hover:bg-violet-600">
+            <a href="https://drive.google.com/file/d/1UGg8oH6_3BD1FoHow3WMu_FBEDIVaakW/view?usp=sharing" className="bg-violet-700 p-4 rounded-2xl hover:bg-violet-600">
               Download CV <i className="ri-download-line ri-lg"></i>
             </a>
 
